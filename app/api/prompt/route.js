e5da605ccad4e11058a19f97c895ec0b5c4f6982
SDE-Tushar-Funity <200030059@iitdh.ac.in>
2024-06-02 22:35:16 +0530

    added console fr debugging

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,9 +6,10 @@ export const GET = async (request) => {
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
-
+    console.log(prompts);
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
+    console.log(error);
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
